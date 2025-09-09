3e37fe395b342b43e1884f7389a7db6ef8fdb976
Sleenjep <alexanderdryomov7@gmail.com>
2025-09-09 19:26:30 +0300

    update main.go

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"flag"
+	"io"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 )
 
@@ -38,6 +40,17 @@ func main() {
 	addr := flag.String("addr", ":4000", "Сетевой адрес HTTP")
 	flag.Parse()
 
+	errorFile, err := os.OpenFile("errors.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer errorFile.Close()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+	mw := io.MultiWriter(os.Stderr, errorFile)
+	errorLog := log.New(mw, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", home)
@@ -45,10 +58,15 @@ func main() {
 	mux.HandleFunc("/snippet/create", createSnippet)
 
 	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static")})
-	mux.Handle("/static", http.NotFoundHandler())
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Printf("Запуск сервера на %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
+	infoLog.Printf("Запуск сервера на %s", *addr)
+	err = srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
