00e800ed90d6242bd37727478ef2c26413fe7fd6
Sleenjep <alexanderdryomov7@gmail.com>
2025-09-09 14:13:42 +0300

    update main.go

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -3,13 +3,46 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
 )
+
+type neuteredFileSystem struct {
+	fs http.FileSystem
+}
+
+func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
+	f, err := nfs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	info, err := f.Stat()
+	if err != nil {
+		_ = f.Close()
+		return nil, err
+	}
+
+	if info.IsDir() {
+		index := filepath.Join(path, "index.html")
+		if _, err := nfs.fs.Open(index); err != nil {
+			_ = f.Close()
+			return nil, err
+		}
+	}
+
+	return f, nil
+}
 
 func main() {
 	mux := http.NewServeMux()
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
+
+	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static")})
+	mux.Handle("/static", http.NotFoundHandler())
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	log.Println("Запуск сервера на http://127.0.0.1:4000")
 	err := http.ListenAndServe(":4000", mux)
