022f352c93c15ce3e46e2eab729fba7d21cc82a3
Sleenjep <alexanderdryomov7@gmail.com>
2025-09-10 22:47:02 +0300

    update main.go

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strings"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 type application struct {
@@ -14,10 +19,29 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", ":4000", "Сетевой адрес веб-сервера")
+
+	passBytes, err := os.ReadFile("../secrets/pg_pass.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	password := strings.TrimSpace(string(passBytes))
+
+	dsn := flag.String("dsn",
+		fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable",
+			"user_pg", password, "localhost", 5432, "snippetbox-pg-db"),
+		"PostgreSQL DSN",
+	)
+
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
 
 	app := &application{
 		errorLog: errorLog,
@@ -31,6 +55,17 @@ func main() {
 	}
 
 	infoLog.Printf("Запуск сервера (http://127.0.0.1:4000/) на %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
