eac34a20966458f84343c734040c19e48e11ab29
Sleenjep <alexanderdryomov7@gmail.com>
2025-09-28 14:40:34 +0300

    update: web was updated

--- snippetbox/cmd/web/templates.go
+++ snippetbox/cmd/web/templates.go
@@ -3,5 +3,6 @@ package main
 import "github.com/Sleenjep/snippetbox-proj/snippetbox/pkg/models"
 
 type templateData struct {
-	Snippet *models.Snippet
+	Snippet  *models.Snippet
+	Snippets []*models.Snippet
 }
